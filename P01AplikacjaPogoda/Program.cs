246f59e050648a078d79d98aaeb7077b7d709b35
tomtom <tomasz.les@pw.edu.pl>
2022-07-17 08:35:12 +0200

    Add project files.

---
+++ P01AplikacjaPogoda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01AplikacjaPogoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nazwaMiasta = "warszawa";
+            string url = $"https://www.google.com/search?q=pogoda+{nazwaMiasta}";
+
+            WebClient wc = new WebClient();
+            string dane=  wc.DownloadString(url);
+
+            File.WriteAllText(@"c:\dane\15z\pogoda.html", dane);
+
+        }
+    }
+}
